d360d615ba14579635c17770b7483c064f00d3cd
zhongzhihong <qingtian88@live.com>
2017-10-19 09:29:35 +0800

    new file:   .babelrc 	new file:   .eslintrc 	new file:   app/app.jsx 	new file:   package.json 	new file:   webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path')
+var webpack = require('webpack')
+var htmlWebpackPlugin = require('webpack-html-plugin')
+
+var ROOT_PATH = path.resolve(__dirname)
+var APP_PATH  = path.resolve(ROOT_PATH,'app')
+var BUILD_PATH= path.resolve(ROOT_PATH,'build')
+
+var entryName = 'app.jsx'
+var fileName = 'bundle.js'
+module.exports = {
+    entry:{
+        app: path.resolve(APP_PATH,entryName)
+    },
+    output:{
+        path:BUILD_PATH,
+        filename:fileName
+    },
+    devtool:'eval-source-map',
+
+    devServer:{
+        historyApiFallback:true,
+        hot:true,
+        inline:true,
+        progress:true
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.jsx?$/,
+                loaders:'eslint-loader',
+                enforce:'pre',
+                include:APP_PATH
+            },
+            {
+                test:/\.jsx?$/,
+                loaders:'babel-loader',
+                include:APP_PATH
+            }
+        ]   
+    },
+    plugins:[
+        new htmlWebpackPlugin({title:'my first react applicaiton'})
+    ],
+    resolve:{
+        extensions:['.js','.jsx']
+    }
+}
